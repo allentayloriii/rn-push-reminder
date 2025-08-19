6938313f6c050541b0810bb957748f388b2a0448
SoundtrkSamurai <furtimat3@yahoo.com>
2025-08-19 16:46:29 -0700

    update

---
+++ ios/Pods/Target Support Files/Galaxies/Galaxies-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Galaxies : NSObject
+@end
+@implementation PodsDummy_Galaxies
+@end
---
+++ ios/Pods/Target Support Files/Pods-rnpushreminder/Pods-rnpushreminder-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_rnpushreminder : NSObject
+@end
+@implementation PodsDummy_Pods_rnpushreminder
+@end
